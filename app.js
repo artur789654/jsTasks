bc8bfa1a1b873084524dec571ef8f06a4889bf8c
artur789654 <artur789654@gmail.com>
2024-07-19 20:05:12 +0300

    changed on arrow function

--- app.js
+++ app.js
@@ -16,20 +16,14 @@ console.log(book);
 // Напишіть функцію, яка приймає два числа і повертає їх суму.
 // Напишіть функцію, яка приймає рядок і повертає його в верхньому регістрі.
 // Напишіть функцію, яка приймає масив чисел і повертає новий масив з квадратами цих чисел.
-function sum(a, b) {
-  return a + b;
-}
+const sum = (a, b)=> a + b;
 console.log(sum(2, 5));
 
-function toUpCase(str) {
-  return str.toUpperCase();
-}
+const toUpCase =(str)=> str.toUpperCase();
 console.log(toUpCase("asafsa"));
 
-function abs(arr) {
-  return arr.map((v) => v * v);
-}
-console.log(abs([2, 3, 4, 5, 67]));
+const sqrt=(arr)=>  arr.map((v) => v * v);
+console.log(sqrt([2, 3, 4, 5, 67]));
 
 // Створіть масив з трьох імен. Додайте нове ім'я до кінця масиву і виведіть його.
 // Видаліть перший елемент масиву і виведіть його.
@@ -55,14 +49,14 @@ prom
 // Створіть асинхронну функцію, яка повертає "Hello, World!" через 1 секунду.
 // Викличте цю функцію і виведіть результат в консоль.
 // Використовуйте try/catch для обробки помилки в асинхронній функції, яка кидає помилку.
-async function sayHello() {
+const sayHello= async () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve("hello world");
     }, 1000);
   });
 }
-async function execute() {
+const execute= async ()=> {
   try {
     console.log(await sayHello());
   } catch (error) {
