d435defe83ccccfdbf7b18277d02f7764da649fb
artur789654 <artur789654@gmail.com>
2024-07-26 17:47:36 +0300

    added task-2 from DOM manipulation

--- app.js
+++ app.js
@@ -1,131 +1,85 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const prevBtn = document.getElementById("prev");
-  const nextBtn = document.getElementById("next");
-  const currentMonthAndYear = document.getElementById("currentMonthAndYear");
-  const calendar = document.getElementById("calendar");
-  const selectedDate = document.getElementById("selected-date");
-  const eventList = document.getElementById("event-list");
-  const addEventForm = document.getElementById("add-event-form");
-  const addEventTitle = document.getElementById("add-event-title");
+  const addTask = document.getElementById("addTask");
+  const addTaskInput = document.getElementById("addTaskInput");
+  const tasks = document.getElementById("tasks");
+  const taskFilter = document.getElementById("taskFilter");
 
-  const currentDate = new Date();
-
-  const getEventsFromStorage = () => {
-    const events = localStorage.getItem("events");
-    return events ? JSON.parse(events) : {};
+  const getTasksFromStorage = () => {
+    const tasks = localStorage.getItem("tasks");
+    return tasks ? JSON.parse(tasks) : [];
   };
-
-  const saveEventsToStorage = (events) => {
-    localStorage.setItem("events", JSON.stringify(events));
+  const saveTasksToStorage = (tasks) => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
-
-  const renderCalendar = () => {
-    calendar.innerHTML = "";
-    const daysOfWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"];
-
-    daysOfWeek.forEach((day) => {
-      const item = document.createElement("div");
-      item.classList.add("calendar-header");
-      item.textContent = day;
-      calendar.appendChild(item);
+  const filterTasks = (tasksFromStorage, filter) => {
+    return tasksFromStorage.filter((task) => {
+      if (filter === "all") return true;
+      if (filter === "done") return task.done;
+      if (filter === "not done") return !task.done;
     });
-
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth();
-
-    const firstDay = new Date(year, month, 1);
-    const lastDay = new Date(year, month + 1, 0);
-    const daysInMonth = lastDay.getDate();
-    let startDay = firstDay.getDay();
-    startDay = startDay === 0 ? 6 : startDay - 1;
-
-    for (let i = 0; i < startDay; i++) {
-      calendar.appendChild(document.createElement("div"));
-    }
-
-    for (let i = 1; i <= daysInMonth; i++) {
-      const day = document.createElement("div");
-      day.classList.add("calendar-day");
-      day.textContent = i;
-      day.dataset.date = `${String(i).padStart(2, 0)}-${String(
-        month + 1
-      ).padStart(2, 0)}-${year}`;
-      day.addEventListener("click", handleDayClick);
-      calendar.appendChild(day);
-    }
-    currentMonthAndYear.innerHTML = `${String(month + 1).padStart(
-      2,
-      0
-    )}-${year}`;
   };
 
-  const handleDayClick = (event) => {
-    document.querySelectorAll(".calendar-day.active").forEach((item) => {
-      item.classList.remove("active");
-    });
-    event.target.classList.add("active");
-    const date = event.target.dataset.date;
-    selectedDate.innerText = date;
-    updateEventList(date);
-  };
+  const renderTasks = () => {
+    const tasksFromStorage = getTasksFromStorage();
+    tasks.innerHTML = "";
+    const filter = taskFilter.value;
 
-  const updateEventList = (date) => {
-    eventList.innerHTML = "";
-    const events = getEventsFromStorage();
-    const dayEvents = events[date] || [];
-    dayEvents.forEach((event, index) => {
-      const dayEvent = document.createElement("li");
-      const parEvent = document.createElement("p");
-      parEvent.textContent = event;
+    const filteredTasks = filterTasks(tasksFromStorage, filter);
+
+    filteredTasks.forEach((task, index) => {
+      const li = document.createElement("li");
+      const par = document.createElement("p");
+      const checkbox = document.createElement("input");
       const deleteBtn = document.createElement("button");
-      deleteBtn.textContent = "Delete";
-      deleteBtn.value = index;
-      deleteBtn.addEventListener("click", (event) => deleteEvent(date, event));
-      dayEvent.appendChild(parEvent);
-      dayEvent.appendChild(deleteBtn);
-      eventList.appendChild(dayEvent);
-    });
 
-    addEventForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      if (selectedDate.textContent !== "Виберіть дату") {
-        const eventInput = addEventTitle.value.trim();
-        if (!eventInput) {
-          return;
-        }
-        const events = getEventsFromStorage();
-        if (!events[date]) {
-          events[date] = [];
-        }
-        events[date].push(eventInput);
-        saveEventsToStorage(events);
-        addEventTitle.value = "";
-        updateEventList(date);
-      }
-    });
+      par.textContent = task.text;
 
-    const deleteEvent = (date, event) => {
-      const events = getEventsFromStorage();
-      const indexDeleteBtn = event.target.value;
-      if (events[date] && indexDeleteBtn !== undefined) {
-        events[date].splice(indexDeleteBtn, 1);
-        if (events[date].length === 0) {
-          delete events[date];
-        }
-      }
-      saveEventsToStorage(events);
-      updateEventList(date);
-    };
+      checkbox.type = "checkbox";
+      checkbox.checked = task.done;
+      checkbox.addEventListener("change", () => toggleTask(task.id));
+
+      deleteBtn.textContent = "delete";
+      deleteBtn.addEventListener("click", () => deleteTask(task.id));
+
+      li.append(checkbox, par, deleteBtn);
+      tasks.appendChild(li);
+    });
   };
 
-  prevBtn.addEventListener("click", () => {
-    currentDate.setMonth(currentDate.getMonth() - 1);
-    renderCalendar();
+  addTask.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskText = addTaskInput.value.trim();
+
+    if (taskText === "") return;
+    const tasksFromStorage = getTasksFromStorage();
+
+    const newTask = {
+      id: new Date().getTime(),
+      text: taskText,
+      done: false,
+    };
+
+    tasksFromStorage.push(newTask);
+    saveTasksToStorage(tasksFromStorage);
+    renderTasks();
+    addTaskInput.value = "";
   });
 
-  nextBtn.addEventListener("click", () => {
-    currentDate.setMonth(currentDate.getMonth() + 1);
-    renderCalendar();
-  });
-  renderCalendar();
+  const toggleTask = (taskId) => {
+    const tasksFromStorage = getTasksFromStorage();
+    const taskIndex = tasksFromStorage.findIndex((task) => task.id === taskId);
+    tasksFromStorage[taskIndex].done = !tasksFromStorage[taskIndex].done;
+    saveTasksToStorage(tasksFromStorage);
+    renderTasks();
+  };
+
+  const deleteTask = (taskId) => {
+    const tasksFromStorage = getTasksFromStorage();
+    const updatedTasks = tasksFromStorage.filter((task) => task.id !== taskId);
+    saveTasksToStorage(updatedTasks);
+    renderTasks();
+  };
+
+  taskFilter.addEventListener("change", renderTasks);
+  renderTasks();
 });
