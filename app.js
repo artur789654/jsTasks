bc657846ad8fce92fe85f5e0f4de432b5b57c30b
artur789654 <artur789654@gmail.com>
2024-07-27 15:40:53 +0300

    added task-3 from DOM manipulation

--- app.js
+++ app.js
@@ -1,85 +1,133 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addTask = document.getElementById("addTask");
-  const addTaskInput = document.getElementById("addTaskInput");
-  const tasks = document.getElementById("tasks");
-  const taskFilter = document.getElementById("taskFilter");
+  const addContact = document.getElementById("addContact");
+  const addName = document.getElementById("addName");
+  const addPhoneNumber = document.getElementById("addPhoneNumber");
+  const searchInput = document.getElementById("searchInput");
+  const contacts = document.getElementById("contacts");
 
-  const getTasksFromStorage = () => {
-    const tasks = localStorage.getItem("tasks");
-    return tasks ? JSON.parse(tasks) : [];
+  const getContactsFromStorage = () => {
+    const contacts = localStorage.getItem("contacts");
+    return contacts ? JSON.parse(contacts) : [];
   };
-  const saveTasksToStorage = (tasks) => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+  const saveContactsToStorage = (contacts) => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
   };
-  const filterTasks = (tasksFromStorage, filter) => {
-    return tasksFromStorage.filter((task) => {
-      if (filter === "all") return true;
-      if (filter === "done") return task.done;
-      if (filter === "not done") return !task.done;
+
+  const renderContacts = () => {
+    const сontactsFromStorage = getContactsFromStorage();
+    contacts.innerHTML = "";
+    const searchValue = searchInput.value.toLowerCase();
+
+    сontactsFromStorage.forEach((contact) => {
+      if (
+        contact.name.toLowerCase().includes(searchValue) ||
+        contact.phone.toString().includes(searchValue)
+      ) {
+        const li = document.createElement("li");
+        const nameSpan = document.createElement("span");
+        const phoneSpan = document.createElement("span");
+        const editBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
+
+        nameSpan.textContent = contact.name;
+        phoneSpan.textContent = contact.phone;
+
+        nameSpan.classList.add("contact-name");
+        phoneSpan.classList.add("contact-phone");
+
+        editBtn.textContent = "Edit";
+        editBtn.classList.add("edit-btn");
+        editBtn.addEventListener("click", () => editContact(contact.id));
+
+        deleteBtn.textContent = "delete";
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.addEventListener("click", () => deleteContact(contact.id));
+
+        li.append(nameSpan, phoneSpan, editBtn, deleteBtn);
+        contacts.appendChild(li);
+      }
     });
   };
 
-  const renderTasks = () => {
-    const tasksFromStorage = getTasksFromStorage();
-    tasks.innerHTML = "";
-    const filter = taskFilter.value;
-
-    const filteredTasks = filterTasks(tasksFromStorage, filter);
-
-    filteredTasks.forEach((task, index) => {
-      const li = document.createElement("li");
-      const par = document.createElement("p");
-      const checkbox = document.createElement("input");
-      const deleteBtn = document.createElement("button");
-
-      par.textContent = task.text;
-
-      checkbox.type = "checkbox";
-      checkbox.checked = task.done;
-      checkbox.addEventListener("change", () => toggleTask(task.id));
-
-      deleteBtn.textContent = "delete";
-      deleteBtn.addEventListener("click", () => deleteTask(task.id));
-
-      li.append(checkbox, par, deleteBtn);
-      tasks.appendChild(li);
-    });
-  };
-
-  addTask.addEventListener("submit", (e) => {
+  addContact.addEventListener("submit", (e) => {
     e.preventDefault();
-    const taskText = addTaskInput.value.trim();
+    const name = addName.value.trim();
+    const phone = addPhoneNumber.value.trim();
 
-    if (taskText === "") return;
-    const tasksFromStorage = getTasksFromStorage();
+    if (name === "" || phone === "") return;
+    const сontactsFromStorage = getContactsFromStorage();
 
-    const newTask = {
+    const newContact = {
       id: new Date().getTime(),
-      text: taskText,
-      done: false,
+      name,
+      phone,
     };
 
-    tasksFromStorage.push(newTask);
-    saveTasksToStorage(tasksFromStorage);
-    renderTasks();
-    addTaskInput.value = "";
+    сontactsFromStorage.push(newContact);
+    saveContactsToStorage(сontactsFromStorage);
+    renderContacts();
+    addName.value = "";
+    addPhoneNumber.value = "";
   });
 
-  const toggleTask = (taskId) => {
-    const tasksFromStorage = getTasksFromStorage();
-    const taskIndex = tasksFromStorage.findIndex((task) => task.id === taskId);
-    tasksFromStorage[taskIndex].done = !tasksFromStorage[taskIndex].done;
-    saveTasksToStorage(tasksFromStorage);
-    renderTasks();
+  const editContact = (contactId) => {
+    const сontactsFromStorage = getContactsFromStorage();
+    const contactIndex = сontactsFromStorage.findIndex(
+      (contact) => contact.id === contactId
+    );
+    const contact = сontactsFromStorage[contactIndex];
+
+    const editNameInput = document.createElement("input");
+    const editPhoneInput = document.createElement("input");
+    const saveBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+
+    editNameInput.type = "text";
+    editNameInput.value = contact.name;
+    editNameInput.classList.add("edit-input");
+
+    editPhoneInput.type = "number";
+    editPhoneInput.value = contact.phone;
+    editPhoneInput.classList.add("edit-input");
+
+    saveBtn.textContent = "Save";
+    saveBtn.classList.add("save-btn");
+    saveBtn.addEventListener("click", () =>
+      saveEditedContact(contactId, editNameInput.value, editPhoneInput.value)
+    );
+
+    cancelBtn.textContent = "cancel";
+    cancelBtn.classList.add("cancel-btn");
+    cancelBtn.addEventListener("click", renderContacts);
+
+    const li = contacts.children[contactIndex];
+    li.innerHTML = "";
+    li.append(editNameInput, editPhoneInput, saveBtn, cancelBtn);
   };
 
-  const deleteTask = (taskId) => {
-    const tasksFromStorage = getTasksFromStorage();
-    const updatedTasks = tasksFromStorage.filter((task) => task.id !== taskId);
-    saveTasksToStorage(updatedTasks);
-    renderTasks();
+  const saveEditedContact = (id, editNameInput, editPhoneInput) => {
+    const сontactsFromStorage = getContactsFromStorage();
+    const contactIndex = сontactsFromStorage.findIndex(
+      (contact) => contact.id === id
+    );
+
+    сontactsFromStorage[contactIndex].name = editNameInput;
+    сontactsFromStorage[contactIndex].phone = editPhoneInput;
+
+    saveContactsToStorage(сontactsFromStorage);
+    renderContacts();
   };
 
-  taskFilter.addEventListener("change", renderTasks);
-  renderTasks();
+  const deleteContact = (contactId) => {
+    const сontactsFromStorage = getContactsFromStorage();
+    const updatedContact = сontactsFromStorage.filter(
+      (contact) => contact.id !== contactId
+    );
+
+    saveContactsToStorage(updatedContact);
+    renderContacts();
+  };
+
+  searchInput.addEventListener("input", renderContacts);
+  renderContacts();
 });
